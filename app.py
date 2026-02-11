15c2c0a8b078135c63448d947c7dc1f900a277fc
Gaurav2007-max <aroraganesh2007@gmail.com>
2026-02-11 18:03:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ model = genai.GenerativeModel('gemini-flash-latest')
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host=os.getenv('MYSQL_HOST', 'localhost'),
+            host=os.getenv('MYSQL_HOST', 'db'),
             user=os.getenv('MYSQL_USER', 'root'),
-            password=os.getenv('MYSQL_PASSWORD', ''),
+            password=os.getenv('MYSQL_PASSWORD', 'root'),
             database=os.getenv('MYSQL_DB', 'career_pulse')
         )
         return connection
